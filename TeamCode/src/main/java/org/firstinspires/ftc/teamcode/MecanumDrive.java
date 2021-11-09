26857173bcf5efd5f61ebc34d7b6f2078c8b3e5a
Anthony Li <me@anli.dev>
2021-11-09 17:35:22 -0500

    Add initial teleop code
    Co-Authored-By: aitak <aitak23@bergen.org>
    Co-Authored-By: Alice Zhang <anz0063@gmail.com>

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MecanumDrive {
+    DcMotor fl, fr, bl, br;
+
+    public MecanumDrive(HardwareMap hmap){
+        fl = hmap.dcMotor.get("fl");
+        fr = hmap.dcMotor.get("fr");
+        bl = hmap.dcMotor.get("bl");
+        br = hmap.dcMotor.get("br");
+    }
+    double[] powerArr = new double[4];
+
+    public void move(double x, double y, double turn) {
+        powerArr[0] = x+y+turn; //fl power
+        powerArr[1] = y-x-turn; //Fr Power
+        powerArr[2] = y-x+turn;//bl Power
+        powerArr[3] = y+x-turn; //br Power
+
+        double max = Math.max(Math.abs(powerArr[0]),Math.abs(powerArr[1]));
+        double tempMax = Math.max(Math.abs(powerArr[2]),Math.abs(powerArr[3]));
+        max = Math.max(max, tempMax);
+
+        if(max>1){
+            //We have to normalize
+            for(int i=0;i<4;i++){
+                powerArr[i]=powerArr[i]/max;
+            }
+        }
+
+        fl.setPower(powerArr[0]);
+        fr.setPower(powerArr[1]);
+        bl.setPower(powerArr[2]);
+        br.setPower(powerArr[3]);
+    }
+    public void driveForwardsWithPower(double power){
+        move(0,power,0);
+    }
+    public void driveBackwardsWithPower(double power){
+        move(0,power,0);
+
+    }
+    public void strafeLeftWithPower(double power){
+        move(-power,0,0);
+    }
+    public void strafeRightWithPower(double power){
+        move(power,0,0);
+    }
+    public void turnLeftWithPower(double power){
+        move(0,0,-power);
+    }
+    public void turnRightWithPower(double power){
+        move(0,0, power);
+    }
+    public void stop(){
+
+        move(0,0,0);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMode.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "Tele-Op Mode")
+public class TeleOpMode extends LinearOpMode {
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+           // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.right_stick_x;
+            // leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            // rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+        }
+    }
+}
