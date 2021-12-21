12195dedcc2356301e300152f623151107328e90
Alice Zhang <anz0063@gmail.com>
2021-12-21 17:06:21 -0500

    i spared the duck this time (drop to warehouse auton)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/DuckSpareAuton.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.odometry.OdometryMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Autonomous(name = "Spare Duck & Warehouse")
+public class DuckSpareAuton extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        OdometryMecanumDrive drive = new OdometryMecanumDrive(hardwareMap);
+
+        waitForStart();
+        TrajectorySequence sequence = drive.trajectorySequenceBuilder(new Pose2d(12, -66, 0))
+                .waitSeconds(0.5)
+                .setTangent(Math.toRadians(90))
+
+                .splineToLinearHeading(new Pose2d(-12, -41,Math.toRadians(-90)), Math.toRadians(90))
+
+                .waitSeconds(0.5)
+                .forward(3)
+                .turn(Math.toRadians(90))
+
+                .lineTo(new Vector2d(15, -44))
+                .build();
+        drive.setPoseEstimate(sequence.start());
+        drive.followTrajectorySequence(sequence);
+    }
+}
