d59f3728961e990aa78ff3c0529d37dfb0c3d597
Ari Kamat <arijkamat@gmail.com>
2022-02-25 17:42:01 -0500

    Add timer to teleop and create generic servo value tester

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/GenericServoTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.test;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+@TeleOp
+public class GenericServoTest extends OpMode {
+    public static String servoName = "carriage";
+    public static double pos = 0.87;
+    Servo servo;
+    @Override
+    public void init() {
+        servo = hardwareMap.get(Servo.class, servoName);
+    }
+
+    @Override
+    public void loop() {
+        servo.setPosition(pos);
+    }
+}
