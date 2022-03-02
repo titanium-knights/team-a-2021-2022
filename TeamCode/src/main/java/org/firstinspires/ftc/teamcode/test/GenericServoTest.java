52cdf7c6f61245aec9b24ff2bd4b7111095e65b2
Ari Kamat <arijkamat@gmail.com>
2022-03-02 18:54:54 -0500

    Add Test OpModes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/GenericServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/GenericServoTest.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.teamcode.test;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @Config
 @TeleOp
@@ -12,6 +16,7 @@ public class GenericServoTest extends OpMode {
     public static String servoName = "carriage";
     public static double pos = 0.87;
     Servo servo;
+    Telemetry dashTelemetry = FtcDashboard.getInstance().getTelemetry();
     @Override
     public void init() {
         servo = hardwareMap.get(Servo.class, servoName);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/MotorEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test/MotorEncoderTest.java
@@ -1,15 +1,20 @@
 package org.firstinspires.ftc.teamcode.test;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @Config
 @TeleOp
 public class MotorEncoderTest extends OpMode {
     DcMotorEx motor;
     public static String motorName = "capstone_arm";
+    Telemetry dashTelemetry = FtcDashboard.getInstance().getTelemetry();
+
     @Override
     public void init() {
         motor = hardwareMap.get(DcMotorEx.class, motorName);
@@ -24,6 +29,8 @@ public class MotorEncoderTest extends OpMode {
             motor.setPower(0);
         }
         telemetry.addData("Motor Position", motor.getCurrentPosition());
+        dashTelemetry.addData("Motor Position", motor.getCurrentPosition());
+
     }
 
 
