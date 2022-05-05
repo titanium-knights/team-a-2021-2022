0d3685ca73ad04a7b922767a1d7fc807a8a948a5
alisona le <alile24@bergen.org>
2022-05-05 00:02:20 -0400

    breaddie

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BreaddieDance.java
@@ -0,0 +1,22 @@
+//package org.firstinspires.ftc.teamcode.auto;
+//
+//import com.acmerobotics.dashboard.config.Config;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.Gamepad;
+//import com.qualcomm.robotcore.hardware.HardwareMap;
+//import org.firstinspires.ftc.teamcode.teleop.BasicPassdionComponent;
+//import org.firstinspires.ftc.teamcode.teleop.PassdionOpMode;
+//import org.jetbrains.annotations.NotNull;
+//
+//
+//
+//public class BreaddieDance extends LinearOpMode {
+//    @Override
+////    public void runOpMode({
+////        motor = hardwareMap.dcMotor.get("capstone");
+////
+////    })
+//
+//
+//}
